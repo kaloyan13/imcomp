82b9ad56954ff5bda7b3c094a8940d71dff0a25b
Abhishek Dutta <thelinuxmaniac@gmail.com>
2018-02-28 17:04:55 +0000

    fixed misspelled define constant

--- include/imcomp_server_config.h
+++ include/imcomp_server_config.h
@@ -8,7 +8,7 @@
 #define IMCOMP_SERVER_VERSION_PATCH "3"
 #define IMCOMP_SERVER_URL "http://www.robots.ox.ac.uk/~vgg/software/imcomp/"
 
-#define IMCOMP_SERVER_AUTHOR_NAME ""
+#define IMCOMP_SERVER_AUTHOR_NAME "Abhishek Dutta"
 #define IMCOMP_SERVER_AUTHOR_EMAIL "adutta@robots.ox.ac.uk"
 #define IMCOMP_SERVER_FIRST_RELEASE_DATE "Nov. 2017"
 #define IMCOMP_SERVER_CURRENT_RELEASE_DATE "02 Mar. 2018"
