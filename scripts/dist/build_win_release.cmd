8a255b7ef382e5159b0660295ae35c52391d6be1
Abhishek Dutta <thelinuxmaniac@gmail.com>
2018-03-01 14:27:32 +0000

    creating windows build script (ongoing), updated CHANGELOG

--- scripts/dist/build_win_release.cmd
+++ scripts/dist/build_win_release.cmd
@@ -1,14 +1,44 @@
+::
 :: Script to build Windows releases
+:: Author: Abhishek Dutta <adutta@robots.ox.ac.uk>
+:: March 01, 2018
+::
 
+@ECHO OFF
+cls
+
+SET CURRENT_PATH=%cd%
+
+SET RELEASE_PACKAGE_NAME="Traherne"
+SET RELEASE_PACKAGE_DESCRIPTION="Traherne Digital Collator"
+SET IMCOMP_SERVER_VERSION_PATCH="3" REM Traherne user interface version
+
+SET BUILD_BASE_FOLDER="G:\build\imcomp\release"
 SET CPACK_WIN_DEPS_DLL_BASE_FOLDER="G:\build\imcomp\deps\cpack_win_deps_dll"
 SET CMAKE_EXEC="c:\Program Files\CMake\bin\cmake.exe"
 SET CMAKE_GENERATOR_NAME="Visual Studio 15 2017"
 SET IMCOMP_SOURCE_PATH="C:\Users\tlm\dev\imcomp"
-SET CMAKE_PREFIX_PATH="C:\Users\tlm\deps\win_x86\lib\include\eigen"
-SET VL_FEAT_LIB_PATH="C:\Users\tlm\deps\win_x86\lib\lib\vl.lib"
-SET VL_FEAT_INCLUDE_DIR="C:\Users\tlm\deps\win_x86\lib\include"
-SET BOOST_ROOT_DIR="C:\Users\tlm\deps\win_x86\boost_1_65_1"
 
-:: First build Win32 release
+::
+:: Build release for Windows 32 bit
+::
+SET BUILD_BASE_FOLDER_WIN32=%BUILD_BASE_FOLDER%\Win32
+mkdir %BUILD_BASE_FOLDER_WIN32%
+cd /d %BUILD_BASE_FOLDER_WIN32%
 
-"c:\Program Files\CMake\bin\cmake.exe" -G "Visual Studio 15 2017" "C:\Users\tlm\dev\imcomp" -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="C:\Users\tlm\deps\win_x86\lib\include\eigen" -DVLFEAT_LIB="C:\Users\tlm\deps\win_x86\lib\lib\vl.lib" -DVLFEAT_INCLUDE_DIR="C:\Users\tlm\deps\win_x86\lib\include" -DBOOST_ROOT="C:\Users\tlm\deps\win_x86\boost_1_65_1"
+:: configure
+%CMAKE_EXEC% ^
+ -G %CMAKE_GENERATOR_NAME% ^
+ -DCMAKE_BUILD_TYPE=Release ^
+ -DRELEASE_PACKAGE_NAME=%RELEASE_PACKAGE_NAME% ^
+ -DIMCOMP_SERVER_VERSION_PATCH=%IMCOMP_SERVER_VERSION_PATCH% ^
+ -DRELEASE_PACKAGE_DESCRIPTION=%RELEASE_PACKAGE_DESCRIPTION% ^
+ -DIMCOMP_SERVER_VERSION_PATCH=%IMCOMP_SERVER_VERSION_PATCH% ^
+ -DVLFEAT_LIB="G:\build\imcomp\deps\deps_source\x86\vlfeat\vlfeat-0.9.21\bin\win32\vl.lib" ^
+ -DVLFEAT_INCLUDE_DIR="G:\build\imcomp\deps\deps_source\x86\vlfeat\vlfeat-0.9.21\" ^
+ -DBOOST_ROOT="G:\build\imcomp\deps\deps_source\x86\boost_1_65_1" ^
+ -DIMCOMP_DEPS_BASEDIR= "G:\\build\\imcomp\\deps\\x86" ^
+ %IMCOMP_SOURCE_PATH%
+
+:: build
+::msbuild PACKAGE.vcxproj /maxcpucount:8 -p:PreferredToolArchitecture=x86 /nologo /p:configuration=Release
